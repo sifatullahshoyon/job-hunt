0e06789a5ebbdc5d2b74c3da767579febeb151c0
freelancersifat627e <freelancersifat627@gmail.comm>
2023-11-28 23:26:01 +0600

    some updated

--- src/components/FeaturedJobs/JobDetails.jsx
+++ src/components/FeaturedJobs/JobDetails.jsx
@@ -4,28 +4,52 @@ import Loading from '../Loading/Loading';
 import { CalendarDaysIcon, CurrencyDollarIcon, EnvelopeIcon, MapPinIcon, PhoneIcon} from '@heroicons/react/24/solid';
 
 const JobDetails = () => {
+    // const [jobData , setJobData] = useState();
+    // const {id} = useParams();
+    // const navigation = useNavigation();
+    // useEffect(() => {
+    //     const loadedData = async () => {
+    //         const res = await fetch('/data/jobs.json');
+    //         const data = await res.json();
+    //         setJobData(data)
+    //     }
+    //     return loadedData;
+    // } , [])
+
+    // if(navigation.state === "loading"){
+    //     <Loading />
+    // };
+
+    // const singleJob = jobData?.find(job => job.id.toString() === id);
+
     const [jobData , setJobData] = useState();
+    const [loading , setLoading] = useState(false);
     const {id} = useParams();
     const navigation = useNavigation();
     useEffect(() => {
-        const loadedData = async () => {
-            const res = await fetch('/data/jobs.json');
-            const data = await res.json();
+        setLoading(true)
+        fetch('/data/jobs.json')
+        .then(res => res.json())
+        .then(data => {
             setJobData(data)
-        }
-        return loadedData;
+            setLoading(false)
+        })
     } , [])
+
+    if(loading){
+        return <Loading />
+    };
 
     if(navigation.state === "loading"){
         <Loading />
     };
 
     const singleJob = jobData?.find(job => job.id.toString() === id);
-    
+
     return (
         <div className='container mx-auto p-5 font-manrope'>
             <div className='md:flex gap-6 mt-32'>
-                <div className='w-full md:w-3/5 h-96 mb-6 p-5'>
+                <div className='w-full md:w-3/5 h-96 mb-8 p-5'>
                     <p className='mb-6 text-gray-500'><small className='font-extrabold text-black'>Job Description:</small> {singleJob?.job_description ? singleJob?.job_description : "Data Not Found"}</p>
                     <p className='mb-6 text-gray-500'><small className='font-extrabold text-black'>Job Responsibility:</small> {singleJob?.job_responsibility ? singleJob?.job_responsibility : "Data Not Found"}</p>
                     <p className='mb-6 text-gray-500'><small className='font-extrabold text-black'>Educational Requirements:</small> {singleJob?.educational_requirements ? singleJob?.educational_requirements : "Data Not Found"}</p>
@@ -37,7 +61,7 @@ const JobDetails = () => {
                         <hr className='my-6 w-full h-[2px] bg-indigo-400' />
                         <div className='flex items-center gap-2 mb-4'>
                             <small ><CurrencyDollarIcon className="h-6 w-6 text-indigo-400 font-light gap-x-2" /></small>
-                            <p className='text-gray-500'><span className='text-xl font-manrope font-bold text-black'>Salary :</span> {singleJob?.salary ? singleJob?.salary : 'Data Not Found'} </p>
+                            <p className='text-gray-500'><span className='text-xl font-manrope font-bold text-black'>Salary :</span> {singleJob?.salary ? singleJob.salary : 'Data Not Found'} </p>
                         </div>
                         <div className='flex items-center gap-2 mb-8'>
                             <small ><CalendarDaysIcon className="h-6 w-6 text-indigo-400 font-light gap-x-2" /></small>
